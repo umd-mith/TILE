9bbf1fd6595943792c8fbe9959a4d7b7de4f1929
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-02-01 13:15:20 +0000

    *Adding colorbar folder
    git-svn-id: http://peach.umd.edu/svn/TILE@17 059f972f-e69e-4bc1-b02e-01f4d4c914a7

---
+++ lib/ColorBar/ColorBar.php
@@ -0,0 +1,16 @@
+	<h3>Color Bar</h3>
+	<div id="colorbar_bkgdisplay" class="bkgDisplay">
+		<label>Current Background Color:</label>
+		<span id="bkg" class="bkgBox"></span>
+	</div>
+	<ul id="colorbar_colorlist"></ul>
+	<h4>Color Sliders</h4>
+	<hr/>
+	<div id="colorbar_sliders" class="ColorBarSliders">
+		<label class="colorbar_slider_label">Red</label>
+		<div id="colorbar_slider_red" class="colorbarslider"></div>
+		<label class="colorbar_slider_label">Green</label>
+		<div id="colorbar_slider_green" class="colorbarslider"></div>
+		<label class="colorbar_slider_label">Blue</label>
+		<div id="colorbar_slider_blue" class="colorbarslider"></div>
+	</div>
