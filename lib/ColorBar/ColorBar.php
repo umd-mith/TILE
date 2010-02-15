75f0be53737bc7186cc1633d517a66242d294ce4
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-02-15 20:31:56 +0000

    *changes to the Image object filterDots and other functions for OCR - using the column by column approach *colorbar now changes using a dotpicker *Box made some style changes
    git-svn-id: http://peach.umd.edu/svn/TILE@39 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/ColorBar/ColorBar.php
+++ lib/ColorBar/ColorBar.php
@@ -6,17 +6,25 @@
 	<ul id="colorbar_colorlist"></ul>
 	<h4>Color Sliders</h4>
 	<hr/>
-	<div id="colorbar_sliders" class="ColorBarSliders">
-		<label class="colorbar_slider_label">Red</label>
-		<div id="colorbar_slider_red" class="colorbarslider"></div>
-		<label class="colorbar_slider_label">Green</label>
-		<div id="colorbar_slider_green" class="colorbarslider"></div>
-		<label class="colorbar_slider_label">Blue</label>
-		<div id="colorbar_slider_blue" class="colorbarslider"></div>
+
+	<div id="colorbar_instructions" class="colorbarInstructions">
+		<span id="colorbar_SETUPBOX">STEP 1: SET OCR REGION</span>
+		
+		<div id="colorbar_sliders" class="ColorBarSliders">
+			<label class="colorbar_slider_label">STEP 1b: SET CONVERSION RGB</label>
+			<br/>
+			<label class="colorbar_slider_label">Red</label>
+			<div id="colorbar_slider_red" class="colorbarslider"></div>
+			<label class="colorbar_slider_label">Green</label>
+			<div id="colorbar_slider_green" class="colorbarslider"></div>
+			<label class="colorbar_slider_label">Blue</label>
+			<div id="colorbar_slider_blue" class="colorbarslider"></div>
+		</div>
+		<span id="colorbar_OCR">STEP 2: OCR</span>
+		<br/>
+		<span id="colorbar_test_rects">STEP 3: SHOW RECTANGLES</span>
+		<hr/>
+		<span id="colorbar_ZOOMin">ZOOM IN</span>
+		<br/>
+		<span id="colorbar_ZOOMout">ZOOM OUT</span>
 	</div>
-	<span id="colorbar_OCR">OCR</span>|
-	<span id="colorbar_test_rects">SHOW RECTANGLES</span>
-	<br/>
-	<span id="colorbar_ZOOMin">ZOOM IN</span>
-	<br/>
-	<span id="colorbar_ZOOMout">ZOOM OUT</span>
