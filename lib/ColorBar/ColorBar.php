426462fbd5637956f7e080bbe2de026f204947d7
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-02-16 21:45:04 +0000

    *added functionality to Color bar *Changed colorbar.php code *now eyedropper works on click on/click off capability *some image.js functionality changed - create resetColorRegion
    git-svn-id: http://peach.umd.edu/svn/TILE@40 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/ColorBar/ColorBar.php
+++ lib/ColorBar/ColorBar.php
@@ -1,10 +1,5 @@
 	<h3>Color Bar</h3>
-	<div id="colorbar_bkgdisplay" class="bkgDisplay">
-		<label>Current Background Color:</label>
-		<span id="bkg" class="bkgBox"></span>
-	</div>
-	<ul id="colorbar_colorlist"></ul>
-	<h4>Color Sliders</h4>
+	
 	<hr/>
 
 	<div id="colorbar_instructions" class="colorbarInstructions">
@@ -12,6 +7,7 @@
 		
 		<div id="colorbar_sliders" class="ColorBarSliders">
 			<label class="colorbar_slider_label">STEP 1b: SET CONVERSION RGB</label>
+			<h4>Color Sliders</h4>
 			<br/>
 			<label class="colorbar_slider_label">Red</label>
 			<div id="colorbar_slider_red" class="colorbarslider"></div>
@@ -19,6 +15,15 @@
 			<div id="colorbar_slider_green" class="colorbarslider"></div>
 			<label class="colorbar_slider_label">Blue</label>
 			<div id="colorbar_slider_blue" class="colorbarslider"></div>
+			<br/>
+			<div id="colorbar_bkgdisplay" class="bkgDisplay">
+				<label>Current Background Color:</label>
+				<br/>
+				<span id="bkg" class="bkgBox"></span>
+			</div>
+			<h4>Eyedropper Tool</h4>
+			<span id="colorbar_eyedropper" class="eyedropper">USE EYEDROPPER<img id="colorbar_ed_img"></img></span>
+			
 		</div>
 		<span id="colorbar_OCR">STEP 2: OCR</span>
 		<br/>
