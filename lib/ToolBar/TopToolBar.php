ece1760a1f28a4641eb7400554c2b6a0c0b93beb
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-02-22 20:44:56 +0000

    *adding Selector Tools and Toolbar to the repository *Adding functions to enhance RaphaelCanvas - now uses more shapes *Toolbars separated into TopToolBar and SideToolBar in order to flow into Elisabeth's interface design
    git-svn-id: http://peach.umd.edu/svn/TILE@49 059f972f-e69e-4bc1-b02e-01f4d4c914a7

---
+++ lib/ToolBar/TopToolBar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+TOPTOOLBAR HTML 
+**/
+
+
+
+?>
+
+<h3>ToolBar</h3>
+<span id="tab1">Do Some Image Stuffs</span>
+<br/>
+<span id="tab2">Do Some Text Stuffs</span>
+<div id="colorbar_instructions" class="colorbarInstructions">
+	<span id="colorbar_SETUPBOX">STEP 1: SET OCR REGION</span>
+	<br/>
+	<span id="colorbar_OCR">STEP 2: OCR</span>
+	<br/>
+	<span id="colorbar_test_rects">STEP 3: SHOW RECTANGLES</span>
+	<hr/>
+	<span id="colorbar_ZOOMin">ZOOM IN</span>
+	<br/>
+	<span id="colorbar_ZOOMout">ZOOM OUT</span>
+</div>
