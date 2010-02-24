bec8e91c8d3d382c6fbdc43f4b488416fb815a3f
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-02-24 15:20:52 +0000

    --JGD-- *changed TopToolBar so it now handles the color and OCR changes - has all ColorBar code in it *TopToolBar.php and SideToolBar.php have changed *ImageConversion.css now has styles associated with different layouts for ImageContainer, mainStage, ToolBar - EngineInit.js also changed to handle this layout
    git-svn-id: http://peach.umd.edu/svn/TILE@53 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/ToolBar/SideToolBar.php
+++ lib/ToolBar/SideToolBar.php
@@ -7,23 +7,3 @@
 ?>
 
 
-<div id="colorbar_sliders" class="ColorBarSliders">
-	<label class="colorbar_slider_label">STEP 1b: SET CONVERSION RGB</label>
-	<h4>Color Sliders</h4>
-	<br/>
-	<label class="colorbar_slider_label">Red</label>
-	<div id="colorbar_slider_red" class="colorbarslider"></div>
-	<label class="colorbar_slider_label">Green</label>
-	<div id="colorbar_slider_green" class="colorbarslider"></div>
-	<label class="colorbar_slider_label">Blue</label>
-	<div id="colorbar_slider_blue" class="colorbarslider"></div>
-	<br/>
-	<div id="colorbar_bkgdisplay" class="bkgDisplay">
-		<label>Current Background Color:</label>
-		<br/>
-		<span id="bkg" class="bkgBox"></span>
-	</div>
-	<h4>Eyedropper Tool</h4>
-	<span id="colorbar_eyedropper" class="eyedropper">USE EYEDROPPER<img id="colorbar_ed_img"></img></span>
-	
-</div>
--- lib/ToolBar/TopToolBar.php
+++ lib/ToolBar/TopToolBar.php
@@ -13,7 +13,26 @@ TOPTOOLBAR HTML
 <span id="tab2">Do Some Text Stuffs</span>
 <div id="colorbar_instructions" class="colorbarInstructions">
 	<span id="colorbar_SETUPBOX">STEP 1: SET OCR REGION</span>
-	<br/>
+	<div id="colorbar_sliders" class="ColorBarSliders">
+		<label class="colorbar_slider_label">STEP 1b: SET CONVERSION RGB</label>
+		<h4>Color Sliders</h4>
+		<br/>
+		<label class="colorbar_slider_label">Red</label>
+		<div id="colorbar_slider_red" class="colorbarslider"></div>
+		<label class="colorbar_slider_label">Green</label>
+		<div id="colorbar_slider_green" class="colorbarslider"></div>
+		<label class="colorbar_slider_label">Blue</label>
+		<div id="colorbar_slider_blue" class="colorbarslider"></div>
+		<br/>
+		<div id="colorbar_bkgdisplay" class="bkgDisplay">
+			<label>Current Background Color:</label>
+			<br/>
+			<span id="bkg" class="bkgBox"></span>
+		</div>
+		<h4>Eyedropper Tool</h4>
+		<span id="colorbar_eyedropper" class="eyedropper">USE EYEDROPPER<img id="colorbar_ed_img"></img></span>
+
+	</div>
 	<span id="colorbar_OCR">STEP 2: OCR</span>
 	<br/>
 	<span id="colorbar_test_rects">STEP 3: SHOW RECTANGLES</span>
