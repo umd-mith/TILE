d4a63e4bbd20954804ac2b643b23801f9aba9c08
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-03-03 21:11:50 +0000

    JGD *changed elements with Ellipse, Drag and Resize for CircleDrag and CircleResize - check for errors *working on Tag.js - need to figure out how this is supposed to work
    git-svn-id: http://peach.umd.edu/svn/TILE@80 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/Tag/Tag.php
+++ lib/Tag/Tag.php
@@ -8,13 +8,13 @@ if(isset($_GET['id'])){
 ?>
 
 <div id="tag_<?php echo $ID; ?>" class="tag">
-	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"></a></h2>
+	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><input id="tagtitleselect_<?php echo $ID; ?>" /></a></h2>
 	<p class="tagedit"><a id="tagedit_<?php echo $ID; ?>" href="#">Edit</a></p>
 	<ul class="tagattrb">
 		<li id="tagobjlabel_<?php echo $ID; ?>" class="tagtype">Object:</li>
 		<li id="tagobjchoice_<?php echo $ID; ?>" class="tagvalue"></li>
 		<li id="tagcoordslabel_<?php echo $ID; ?>" class="tagtype">Coordinates:</li>
-		<li id="tagcoordsvalue_<?php echo $ID; ?>" class="tagvalue"></li>
+		<li id="tagcoordsvalue_<?php echo $ID; ?>" class="tagvalue"><a href="#" class="tagcoords closed">Set the object area</li>
 	</ul>
 </div>
 <?php } ?>
