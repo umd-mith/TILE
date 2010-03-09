c2848153d002d492032f0d9538bac8eca4970a4a
ekvernen <ekvernen@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-03-09 18:29:23 +0000

    git-svn-id: http://peach.umd.edu/svn/TILE@91 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/Tag/Tag.php
+++ lib/Tag/Tag.php
@@ -11,8 +11,8 @@ if(isset($_GET['id'])){
 	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><input id="tagtitleselect_<?php echo $ID; ?>" /></a><a id="tagdel_<?php echo $ID; ?>" class="tagdel btnIcon" href="#"></a></h2>
 	<p class="tagedit"><a id="tagedit_<?php echo $ID; ?>" href="#">Edit</a></p>
 	<ul class="tagattrb">
-		<li id="tagobjlabel_<?php echo $ID; ?>" class="tagtype">Object:</li>
-		<li id="tagobjchoice_<?php echo $ID; ?>" class="tagvalue"><input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li>
+		<li id="tagobjlabel_<?php echo $ID; ?>" class="tagtype">Object: <input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li>
+		<!-- ><li id="tagobjchoice_<?php echo $ID; ?>" class="tagvalue"><input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li> -->
 		<li id="tagcoordslabel_<?php echo $ID; ?>" class="tagtype">Coordinates:</li>
 		<li id="tagcoordsvalue_<?php echo $ID; ?>" class="tagvalue"><a href="#" id="tagCoords_<?php echo $ID; ?>" class="tagcoords closed">Set the object area</li>
 		<li id="tagEditDone_<?php echo $ID ?>"><a href="#">Done</a></li>
