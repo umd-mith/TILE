0117b1612d8b75554fc6f0b7fa31c034c7aac67f
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-03-10 19:10:45 +0000

    JGD
    *making some changes to how TabBar and Tag communicate with shapes, image
    
    git-svn-id: http://peach.umd.edu/svn/TILE@95 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/Tag/Tag.php
+++ lib/Tag/Tag.php
@@ -11,10 +11,10 @@ if(isset($_GET['id'])){
 	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><input id="tagtitleselect_<?php echo $ID; ?>" /></a><a id="tagdel_<?php echo $ID; ?>" class="tagdel btnIcon" href="#"></a></h2>
 	<p class="tagedit"><a id="tagedit_<?php echo $ID; ?>" href="#">Edit</a></p>
 	<ul id="tagAtr_<?php echo $ID; ?>" class="tagattrb">
-		<li id="tagobjlabel_<?php echo $ID; ?>" class="tagtype">Object: <input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li>
+		<!-- ><li id="tagobjlabel_<?php echo $ID; ?>" class="tagtype">Object: <span id="tagobjInputLabel1_<?php echo $ID; ?>"></span><input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li> <-->
 		<!-- ><li id="tagobjchoice_<?php echo $ID; ?>" class="tagvalue"><input id="tagObjInput_<?php echo $ID; ?>" class="tagObjInput" type="text"></input></li> -->
 		<li id="tagcoordslabel_<?php echo $ID; ?>" class="tagtype">Coordinates:</li>
-		<li id="tagcoordsvalue_<?php echo $ID; ?>" class="tagvalue"><a href="#" id="tagCoords_<?php echo $ID; ?>" class="tagcoords closed">Set the object area</li>
+		<li id="tagcoordsvalue_<?php echo $ID; ?>" class="tagvalue"><span id="tagcoordstextoutput_<?php echo $ID; ?>"></span><a href="#" id="tagCoords_<?php echo $ID; ?>" class="tagcoords closed">Set the object area</li>
 	</ul>
 </div>
 <?php } ?>
