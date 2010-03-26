5ffe1169e8e57cacf5f052094afb578d0b7eda6c
ekvernen <ekvernen@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-03-26 16:14:39 +0000

    git-svn-id: http://peach.umd.edu/svn/TILE@123 059f972f-e69e-4bc1-b02e-01f4d4c914a7

---
+++ lib/ToolBar/TabBar.php
@@ -0,0 +1,24 @@
+<?php
+
+//For use in TabBar.js 
+
+?>
+
+<div id="tabs">
+	<ul>
+		<li><a href="#tabs-1">Tagging</a></li>
+		<li><a href="#tabs-2">Recognition</a></li>
+	</ul>
+
+	<div id="tabs-1">
+		<!-- temporary holder for the add tag button -->
+		<a id="addTag"><span class="btnIcon newImg"></span> <span class="button">Add a new tag</span></a>
+		<a id="tagChoice"><select id="tagChoiceList"><option id="opt1">Cat</option><option id="op2">Dog</option><option id="op3">Cheetah</option><option id="op4">Lion</option><option id="op5">Bear</option></select></a>
+	</div>
+
+
+	<div id="tabs-2">
+		
+	</div>
+
+</div>
