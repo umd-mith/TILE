6e4befebc8eeb7a9250b19642a0eac57c19ff585
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-03-26 13:51:04 +0000

    JGD
    *deleted TabBar.php -- using .html now
    *using tagHolder to attach Tags
    
    git-svn-id: http://peach.umd.edu/svn/TILE@119 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/ToolBar/TopToolBar.php
+++ lib/ToolBar/TopToolBar.php
@@ -1,12 +1,3 @@
-<?php
-/**
-*TOPTOOLBAR HTML 
-*
-* HTML: Elisabeth Kvernen
-**/
-
-?>
-
 <div id="topToolbar">
 	<ul class="options">
 		<li id="newimage"><a href="#"><span  class="button">New Image</span> <span class="btnIcon newImg"></span></a></li>
@@ -15,47 +6,3 @@
 		<li id="help"><a href="#"><span  class="button">Help</span> <span class="btnIcon help"></span></a></li>
 	</ul>
 </div>
-
-<!--><h3>ToolBar</h3>
-<span id="tab1" class="toolbar_mode">Do Some Image Stuffs</span>
-<br/>
-<span id="tab2" class="toolbar_mode">Do Some Text Stuffs</span>
-<div id="colorbar_instructions" class="colorbarInstructions">
-	<span id="colorbar_SETUPBOX">STEP 1: SET OCR REGION</span>
-	<div id="colorbar_sliders" class="ColorBarSliders">
-		<label class="colorbar_slider_label">STEP 1b: SET CONVERSION RGB</label>
-		<h4>Color Sliders</h4>
-		<br/>
-		<label class="colorbar_slider_label">Red</label>
-		<div id="colorbar_slider_red" class="colorbarslider"></div>
-		<label class="colorbar_slider_label">Green</label>
-		<div id="colorbar_slider_green" class="colorbarslider"></div>
-		<label class="colorbar_slider_label">Blue</label>
-		<div id="colorbar_slider_blue" class="colorbarslider"></div>
-		<br/>
-		<div id="colorbar_bkgdisplay" class="bkgDisplay">
-			<label>Current Background Color:</label>
-			<br/>
-			<span id="bkg" class="bkgBox"></span>
-		</div>
-		<h4>Eyedropper Tool</h4>
-		<span id="colorbar_eyedropper" class="eyedropper">USE EYEDROPPER<img id="colorbar_ed_img"></img></span>
-
-	</div>
-	<span id="colorbar_OCR">STEP 2: OCR</span>
-	<br/>
-	<span id="colorbar_test_rects">STEP 3: SHOW RECTANGLES</span>
-	<hr/>
-	
-</div>
-<div class="zoomcontrols">
-	<span id="colorbar_ZOOMin" class="zoomcontrol">ZOOM IN</span>
-	<br/>
-	<span id="colorbar_ZOOMout" class="zoomcontrol">ZOOM OUT</span>
-</div>
-<div class="shapes">
-	<button id="rect" class="testcontrol">Rectangle</button>
-	<button id="elli" class="testcontrol">Ellipse</button>
-	<button id="poly" class="testcontrol">Polygon</button>
-</div>
-<-->
