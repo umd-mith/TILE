99acc2787e0cb1b9162e6446afe717e2b38df741
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-04-22 20:16:56 +0000

    JGD
    **new behavior for Tags and NameValue pairs
    
    git-svn-id: http://peach.umd.edu/svn/TILE@169 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/Tag/Tag.php
+++ lib/Tag/Tag.php
@@ -10,6 +10,8 @@ if(isset($_GET['id'])){
 	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><span></span></a><input id="tagTitleInput_<?php echo $ID; ?>" value="" type="text" class="tagTitleInput edit"></input><a id="tagdel_<?php echo $ID; ?>" class="tagdel btnIcon" href="#"></a></h2>
 	
 	<a id="tagedit_<?php echo $ID; ?>" class="tagedit btnIcon unlock" href="#"></a>
-	<ul id="tagAtr_<?php echo $ID; ?>" class="tagattrb"><a id="tagChoice_<?php echo $ID; ?>" class="tagChoice"><select id="tagChoiceList_<?php echo $ID; ?>" class="tagChoiceList"></select></a></ul>
+	<ul id="tagAtr_<?php echo $ID; ?>" class="tagattrb">
+	<!--	<a id="tagChoice_<?php //echo $ID; ?>" class="tagChoice"><select id="tagChoiceList_<?php //echo $ID; ?>" class="tagChoiceList"></select></a> -->
+		</ul>
 </div>
 <?php } ?>
