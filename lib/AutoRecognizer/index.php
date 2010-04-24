e50f792a1949e4e2cee8503962ddf1deebf9d41c
dreside <dreside@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-04-24 13:45:44 +0000

    git-svn-id: http://peach.umd.edu/svn/TILE@171 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/AutoRecognizer/index.php
+++ lib/AutoRecognizer/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <HEAD>
 <link type="text/css" href="../jquery/js/css/ui-lightness/jquery-ui-1.7.2.custom.css" rel="Stylesheet" />
@@ -6,7 +6,10 @@
 		<link type="text/css" href="../../skins/ImageConversion.css" rel="stylesheet" />
   
 	<STYLE type="text/css">
-	
+		#header{
+			background-color: black;
+		}
+		
 		img{
 			display:none;
 		}
@@ -22,6 +25,8 @@
 			position:absolute;
 				top: 50px;
 			left:305px;
+			width: 500px;
+			height: 500px;
 			
 			
 		}
@@ -42,15 +47,18 @@
 			margin:13px 0 10px 0px;
 		}
 		/**PlainBox**/
-
+		#regionBox{
+		display: none;
+		}
+		
 		.boxer_plainbox{
 			position:absolute;
 			left:0px;
 			top:17px;
 			background-color:transparent;
 			border: 2px solid #ff510b;
-			width:100px;
-			height:100px;
+			width:200px;
+			height:300px;
 			margin-right:5px;
 			padding-right:5px;
 		}
@@ -106,11 +114,11 @@
 	
 	<script type="text/javascript" src="../Extensible/Monomyth.js"></script>
 	<script type="text/javascript" src="../Box/Box.js"></script>
-	<script type="text/javascript" src="../Image/Image.js"></script>
+	<script type="text/javascript" src="./AutoRecImage.js"></script>
 	<script type="text/javascript" src="../Shape/Shape.js"></script>
 	<script type="text/javascript" src="./AutoRecognizer.js"></script>
 	<script type="text/javascript" src="../ColorFilter/ColorFilter.js"></script>
-
+	<script type="text/javascript" src="../RegionRule/RegionRule.js"></script>
 	<SCRIPT type="text/JavaScript" language="JavaScript">
 
 	var imageList = [{ uri: "../../Images/ham.jpg",
@@ -130,37 +138,151 @@
 										              "line","line","line","line","line","line",
 										              "line","line","line","line","line","line"]}	              
 					];              
-	
 
+	//  Region 
+	var regionList = [];
+	
+	var regionList = [{ move: "stay", top: "50%", left: "30%", width: "10%", height: "5%"},
+	                  { move: "next", top: "50%", left: "30%", width: "10%", height: "5%"}];
+
+	                   
+						
+						
+	
+	
+	var colorPanel = "<span id='colorPanel' class='color'>"+
+	"<label>Red:</label>"+
+	"<div id='red'></div>"+
+	"<label>Green:</label>"+	            	
+	"<div id='green'></div>"+
+	"<label>Blue:</label>"+
+	"<div id='blue'></div>"+	
+	"</span>"+
+	"<div id='backgroundimage'>0,0,0</div>";
 	var curPageState=0; /*
-						Page states: 0= Setting box for default
-									 1= Setting default color 
-									 				threshold
-									 2= Adjusting box
-									 3= Adjusting color
-									 4= Adjusting lines				
-					 */
-	var pageStates = [
-	         {
-		         
-	         }
-								
-						]
+	Page states: 0= Setting box for default
+				 1= Setting default color 
+				 				threshold
+				 2= Adjusting box
+				 3= Adjusting color
+				 4= Adjusting lines				
+ */
+pageStates = [
+// 0:  Defining boundary boxes
+{
+"sidebar": "<div class='sbMessage'>Please move and resize the red "+
+		  "box to enclose a column of text</div>"+
+		  "<div id='nextButton' class='nextButton'>Next</div>",
+"callback": function(){
+		
+			  showBoundingBox();
+			  $('#nextButton').bind("click",function(){	
+				  
+				  storeRegion();
+				 // changeState(2);
+			});}
+		 
+
+}];
 	
+/* 1:  Setting default color threshold of box
+{
+sidebarHTML:  "<div></div>"
+ 
+}
+				
+	];*/
+	var sidebars = [
+	            	// 1 :  Defining box boundaries
+	                "<div class='sbMessage'>Please move and resize the red box to enclose a column of text</div>"+
+	                "<div class='nextButton'>Next</div>",
+	                // 2:  Adjust default color
+	                "<div class='sbMessage'>Adjust the color settings so that only the text is visible</div>"+
+	     			colorPanel+
+	     			 "<div class='nextButton'>Next</div>",
+					// 3:  Adjust the red box position
+	                "<div class='sbMessage'>Please adjust the red box if necessary the red box to enclose a column of text.</div>"+
+	                "<div class='nextButton'>Next</div>",
+	                // 4:  Adjust the color
+	                 "<div class='sbMessage'>Adjust the color settings so that only the text is visible</div>"+
+	     			colorPanel+
+	     			 "<div class='nextButton'>Next</div>",
+	     			// 5:  Adjust the lines
+	     			"<div class='sbMessage'>Adjust the color settings so that only the text is visible</div>"+
+	     			 "<div class='nextButton'>Next</div>"
+	     			];
+	     			
+	                
+	               
+	                
+	                
+	            	
+	                
+	                
 	function changeState(stateVal){
-
+				state = pageStates[stateVal];
+				$("#sidebarContent").html(state.sidebar);
+				state.callback();
+				
 		}				
-
-	   
+	function showBoundingBox(){
+		$("#regionBox").css("display","block");
+	}
+	function storeRegion(){
+		var pos = $("#regionBox").draggable().position();
+		//var size = $("#regionBox").resizable("option","size");
+		var totWidth = $("#regionBox").parent().width();
+		var totHeight = $("#regionBox").parent().height();
+		var left = pos.left/totWidth;
+		var top = pos.top/totHeight;
+		var width = $("#regionBox").width()/totWidth;
+		var height = $("#regionBox").height()/totHeight;
+		alert(left+","+top+","+width+","+height);
+		}
+	
+	function loadImages(path){
+		imageList = [{ uri: "../../Images/ham.jpg",
+			transcript: [ "line","line","line","line","line","line",
+			              "line","line","line","line","line","line",
+			              "line","line","line","line","line","line",
+			              "line","line","line","line","line","line",
+			              "line","line","line","line","line","line",
+			              "line","line","line","line","line","line",
+			              "line","line","line","line","line","line"]},
+			{ uri: "../../Images/hamTitle.jpg",
+								transcript: [ "line","line","line","line","line","line",
+								              "line","line","line","line","line","line",
+								              "line","line","line","line","line","line",
+								              "line","line","line","line","line","line",
+								              "line","line","line","line","line","line",
+								              "line","line","line","line","line","line",
+								              "line","line","line","line","line","line"]}	              
+			]; 
+		CANVAS.setUpCanvas("../../Images/ham.jpg"); 
+		}   
 	$(document).ready(function() {
 		var container=$("#workspace");
 		CANVAS=new CanvasImage({
 			loc:container
 		});
-		colorPicker = new TileColorFilter({DOM: "colorPanel",red: "red",green: "green",blue: "blue",rgbDiv: "backgroundimage"});
-	   
+		$("#regionBox").resizable({
+			/*stop: function(event,ui){
+
+			$("#boxSize").trigger("boxResized",[ui.size]);
+			}*/
+		
+		});
+		$("#regionBox").draggable({ containment: 'parent',
+									/*stop: function(event,ui){
+									$("#boxSize").trigger("boxMoved",[ui.position]);
+		}*/
+			 });
+		changeState(0);
+		//colorPicker = new TileColorFilter({DOM: "colorPanel",red: "red",green: "green",blue: "blue",rgbDiv: "backgroundimage"});
+
 	  });
 
+	
 		
 
 
@@ -184,24 +306,20 @@
 <div id="content">
 
 	<div id="sidebar">
-	Sidebar
-	<span id="colorPanel" class="color">
-	<label>Red:</label>
-	<div id="red"></div>
-	<label>Green:</label>
-	<div id="green"></div>
-	<label>Blue:</label>
-	<div id="blue"></div>
-	</span>
-
-		<div id="backgroundimage">0,0,0</div>
+		<div id="sidebarHead">
+			<label for="imageManifest">Images uri:</label>
+			<input type="text" size=10 onblur="loadImages(this.value)"></input>
+			<div id="boxSize"></div>
+			<div id="boxPos"></div>
+		</div>
+		<div id="sidebarContent">
+		</div>
 	</div>
 	
 	<div id="workspace">
-		Workspace
-		<img id="srcForCanvas">
+		<img id="srcImageForCanvas" src=""/>
 		</img>
-		
+		<div id="regionBox" class="boxer_plainbox"></div>
 	</div>
 
 </div>
