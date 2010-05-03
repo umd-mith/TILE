123f5191ce925e8f22d36c87dbb9792ed8025bf4
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-05-03 20:11:59 +0000

    JGD
    *setting VectorDrawer to size down so that users can use scrollingimages and colorpicker
    *changing behavoir for Tags
    
    git-svn-id: http://peach.umd.edu/svn/TILE@200 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- lib/Tag/Tag.php
+++ lib/Tag/Tag.php
@@ -6,12 +6,13 @@ if(isset($_GET['id'])){
 	$ID=preg_replace("/tag/","",$_GET['id']);
 	
 ?>
-<div id="tag_<?php echo $ID; ?>" class="tag">
-	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><span></span></a><input id="tagTitleInput_<?php echo $ID; ?>" value="" type="text" class="tagTitleInput edit"></input><a id="tagdel_<?php echo $ID; ?>" class="tagdel btnIcon" href="#"></a></h2>
+
+<li id="tag_<?php echo $ID; ?>" class="tag">
+	<h2><a id="tagtitle_<?php echo $ID; ?>" href="#" class="tagtitle open"><span></span></a><input id="tagTitleInput_<?php echo $ID; ?>" value="" type="text" class="tagTitleInput edit"></input><a id="tagdel_<?php echo $ID; ?>" class="tagdel btnIcon" href="#"></a><a id="<?php echo $ID."_moveMe"; ?>" class="moveTag">MOVE</a></h2>
 	
 	<a id="tagedit_<?php echo $ID; ?>" class="tagedit btnIcon unlock" href="#"></a>
 	<ul id="tagAtr_<?php echo $ID; ?>" class="tagattrb">
 	<!--	<a id="tagChoice_<?php //echo $ID; ?>" class="tagChoice"><select id="tagChoiceList_<?php //echo $ID; ?>" class="tagChoiceList"></select></a> -->
 		</ul>
-</div>
+</li>
 <?php } ?>
