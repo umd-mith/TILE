0495b12351f6a96ce484ee8316676c0511ee8c3b
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-07-05 16:41:21 +0000

    JGD
    git-svn-id: http://peach.umd.edu/svn/TILE/trunk@396 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- PHP/isJSON.php
+++ PHP/isJSON.php
@@ -2,8 +2,10 @@
 include_once('session.php');
 
 if(isset($_SESSION["json"])){
-	echo stripslashes($_SESSION["json"]);
-	//end session
-	session_destroy();
-}
+	echo $_SESSION["json"];
+	
+} 
+//end session
+session_destroy();
+
 ?>
