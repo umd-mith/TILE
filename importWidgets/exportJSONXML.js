bb25170e5e16485910c677be7332e7e6a2ad9083
dreside <dreside@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-11-05 17:48:19 +0000

    git-svn-id: http://peach.umd.edu/svn/TILE/1.5@803 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- importWidgets/exportJSONXML.js
+++ importWidgets/exportJSONXML.js
@@ -9,111 +9,57 @@
 function exportToTEI(JSON_str){
 	
 	var JSONobj = JSON.parse(JSON_str);
-	var str="";
-	src = JSONobj["sourceFile"];
-	// if(__v) console.log("exportToTEI src: "+src);
-	if ((JSONobj["pages"]!=null)&&(JSONobj["pages"].length>0)){
-		var xml="<?xml version=\"1.0\"?>\n<sourceFile>"+(JSONobj['sourceFile'])?JSONobj['sourceFile']:"None given"+"</sourceFile>\n<pages>";
-		
-		function recursiveAddData(str,obj){
-			for(var part in obj){
-				str+="<"+part+">";
-				if(typeof obj[part]=='object'){
-					str=recursiveAddData(str,obj[part]);
-				} else {
-					str+=obj[part];
-				}
-				
-				
-				str+="</"+part+">\n";
-			}
-		}
-		// now we retrieve the pages
-		// Each page receives one XML tag
-		for(var p in JSONobj["pages"]){
-			xml+="<page url=\""+JSONobj["pages"][p]["url"]+"\" info=\""+JSON.stringify(JSONobj["pages"][p]["info"])+"\">\n";
-			for(var l in JSONobj["pages"][p]["lines"]){
-				var line=JSONobj["pages"][p]["lines"];
-				xml+="<line id=\""+(line["id"])?line["id"]:"null"+"\">\n<text>"+line["text"]+"</text>\n<info>"+line["info"]+"</info>\n";
-				for(var extraLineData in line){
-					if(/info|text|id/.test(extraLineData)) continue;
-					// create tag for the extra line data (shapes, selections, things made by a plugin)
-					xml+="<"+extraLineData+">\n";
-					for(var part in line[extraLineData]){
-						xml+="<el"+part+">\n";
-						
-					}
-					xml+="</"+extraLineData+">\n";
-				}
-			}
-			// get other data 
-			for(var ext in JSONobj["pages"][p]){
-				if(/id|info|url|lines/.test(ext)) continue;
-				xml+="<"+ext+">";
-				if(typeof JSONobj["pages"][p][ext]=='object'){
-					recursiveAddData(xml,JSONobj["pages"][p][ext]);
-				} else {
-					xml+=JSONobj["pages"][p][ext];
-				}
-				xml+="</"+ext+">";
-			}
-			xml+="</page>";
-		}
-		
-		
-		// $.ajax({url: src, 
-		// 			success: function(xml){
-		// 			var f = $(xml).find("facsimile").eq(0);
-		// 			
-		// 			for (var i=0;i<JSONobj["pages"].length;i++){
-		// 				
-		// 			 	var page = JSONobj["pages"][i];
-		// 				 var fac = page["info"]["facs"];
-		// 				 var surface = f.find("surface[xml|id='"+fac+"']");
-		// 				 for (var j=0;j<page["lines"].length;j++){
-		// 				 	var ln = page["lines"][j];
-		// 					shapes = ln["shapes"];
-		// 					if (shapes != null) {
-		// 						for (var k = 0; k < shapes.length; k++) {
-		// 							// replave with bounding box soon
-		// 							if (shapes[k]["type"] == "rect") {
-		// 								var pos = shapes[k]["posInfo"];
-		// 								var id = shapes[k]["id"];
-		// 								
-		// 								var zone = "<zone xml:id=\"" + id + "\" ulx=\"" + pos["x"] + "\" uly=\"" + pos["y"] + "\" lrx=\"" + parseFloat(pos["x"]) + parseFloat(pos["width"]) + "\" lry=\"" + parseFloat(pos["y"]) + parseFloat(pos["height"]) + "\"/>";
-		// 								
-		// 								$(surface).eq(0)[0].appendChild($(zone)[0]);
-		// 							
-		// 								var path = ln["info"];
-		// 								offset = path.indexOf("[")+1;
-		// 						
-		// 								len = path.indexOf("]");
-		// 						
-		// 					
-		// 								num = parseInt(path.substring(offset,len),10);
-		// 							
-		// 								lb = $(xml).find("lb").eq(num);
-		// 								lb.attr("facs",id);
-		// 							
-		// 								str = (new XMLSerializer()).serializeToString($(xml)[0]);
-		// 	 							//export out through a jQuery trigger
-		// 								// if(__v) console.log("str is: "+str);
-		// 							}
-		// 						
-		// 						}
-		// 					}
-		// 				 }
-		// 			  
-		// 			}
-		
-			// if(str.length==0){
-			// 	str = (new XMLSerializer()).serializeToString($(xml)[0]);	
-			// } 
-			// Export out the finished string through a trigger
-			$("body:first").trigger("exportStrDone",[xml]);
+	xml = "<tile>"+json2xml(JSONobj)+"</tile>";
+	$("body:first").trigger("exportStrDone",[xml]);
 		
 }
 	
-	//return str;
+
+
+/* JSON 2 XML parser */
+/*	This work is licensed under Creative Commons GNU LGPL License.
+
+	License: http://creativecommons.org/licenses/LGPL/2.1/
+   Version: 0.9
+	Author:  Stefan Goessner/2006
+	Web:     http://goessner.net/ 
+*/
+function json2xml(o, tab) {
+   var toXml = function(v, name, ind) {
+      var xml = "";
+      if (v instanceof Array) {
+         for (var i=0, n=v.length; i<n; i++)
+            xml += ind + toXml(v[i], name, ind+"\t") + "\n";
+      }
+      else if (typeof(v) == "object") {
+         var hasChild = false;
+         xml += ind + "<" + name;
+         for (var m in v) {
+            if (m.charAt(0) == "@")
+               xml += " " + m.substr(1) + "=\"" + v[m].toString() + "\"";
+            else
+               hasChild = true;
+         }
+         xml += hasChild ? ">" : "/>";
+         if (hasChild) {
+            for (var m in v) {
+               if (m == "#text")
+                  xml += v[m];
+               else if (m == "#cdata")
+                  xml += "<![CDATA[" + v[m] + "]]>";
+               else if (m.charAt(0) != "@")
+                  xml += toXml(v[m], m, ind+"\t");
+            }
+            xml += (xml.charAt(xml.length-1)=="\n"?ind:"") + "</" + name + ">";
+         }
+      }
+      else {
+         xml += ind + "<" + name + ">" + v.toString() +  "</" + name + ">";
+      }
+      return xml;
+   }, xml="";
+   for (var m in o)
+      xml += toXml(o[m], m, "");
+   return tab ? xml.replace(/\t/g, tab) : xml.replace(/\t|\n/g, "");
 }
 
