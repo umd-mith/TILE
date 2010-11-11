77773e5999b3df26f57083c22efbf0858dac36fb
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-11-11 19:21:47 +0000

    JGD
    * Using tilevars.php instead of TILECONSTANTS.php
    
    git-svn-id: http://peach.umd.edu/svn/TILE/trunk@863 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- tilevars.php
+++ tilevars.php
@@ -5,12 +5,22 @@
 
 
 $m=$_SERVER['HTTP_REFERER'];
-if(preg_match('/index.html$/',$m)){
-	$host=$_SERVER['HTTP_HOST'];
-	$prefix=preg_replace();
+# TODO: is this valid?
+if(preg_match('/\.php$|\.js$/',$m)==0){
+	$host='http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+	
+	$prefix=preg_replace('/tilevars\.php/','',$host);
+	$prefix=preg_replace('/\//','\/',$prefix);
 	
 	# echo out the JSON
-	
+	echo "{\"hamLoad\":\"".$prefix."importWidgets\/importXML.php?uri=".$prefix."importWidgets\/testHam.xml&rname=text&rnum=0&ipath=".$prefix."Images\/\",
+	\"archLoad\":\"".$prefix."importWidgets\/importXML.php?uri=".$prefix."importWidgets\/testArch.xml&rname=text&rnum=0&ipath=http:\/\/archimedespalimpsest.net\/Data\/065r-072v\/\",
+	\"importDefault\":\"".$prefix."html\/testList.txt\",
+	\"exportDefault\":\"".$prefix."importWidgets\/TEIexport.js\",
+	\"pemLoad\":\"".$prefix."importWidgets\/importXML.php?uri=".$prefix."importWidgets\/testPemb.xml&rname=text&rnum=0&ipath=http:\/\/www.stoa.org\/Pembroke25\/Website-tv\/IMT\/\",
+	\"jsLoad\":\"http:\/\/peach.umd.edu\/staff\/jdickie\/swinburne.json\",
+	\"locJS\":\"".$prefix."html\/hooptydamndoo.js\"
+	}";
 	
 	
 }
