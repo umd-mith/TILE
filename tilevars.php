e119f26865d9bcc13072b3bf2f210db87cb41fb1
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-11-11 18:25:19 +0000

    JGD
    * Forgot to add tilevars.php 
    
    * Fixing helpbox listeners
    
    git-svn-id: http://peach.umd.edu/svn/TILE/trunk@862 059f972f-e69e-4bc1-b02e-01f4d4c914a7

---
+++ tilevars.php
@@ -0,0 +1,19 @@
+<?php
+
+# supply interface with premade TILE data
+# Replaces: TILECONSTANTS.json
+
+
+$m=$_SERVER['HTTP_REFERER'];
+if(preg_match('/index.html$/',$m)){
+	$host=$_SERVER['HTTP_HOST'];
+	$prefix=preg_replace();
+	
+	# echo out the JSON
+	
+	
+	
+}
+
+
+?>
