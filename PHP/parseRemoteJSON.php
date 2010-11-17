3164a13f13e086d85542b436a7fe405dbb38a517
jdickie <jdickie@059f972f-e69e-4bc1-b02e-01f4d4c914a7>
2010-11-17 21:00:03 +0000

    JGD
    * Updated the JSON parser in PHP - having bugs
    
    git-svn-id: http://peach.umd.edu/svn/TILE/trunk@888 059f972f-e69e-4bc1-b02e-01f4d4c914a7

--- PHP/parseRemoteJSON.php
+++ PHP/parseRemoteJSON.php
@@ -12,7 +12,7 @@ if(preg_match("/^<[A-Za-z]*/",$file)>0){
 	die("Error parsing ".$file);
 }
 
-getData($file);
+
 # Gets contents of the [safe] url and 
 # then echos them out 
 function getData($url){
@@ -24,11 +24,24 @@ function getData($url){
 	echo $content;
 	#close stream
 	fclose($fp);
-	
-	
 }
 
+function getScript($url){
+	# go to script page
+	header('Location: '.$url);
+}
+$file=preg_replace('/_AND_/',"&",$file);
 
+if(preg_match("/\.txt$|\.json$|\.TXT$|\.JSON$/",$file)>0){
+	# some kind of text file - get it
+	getData($file);
+}
+
+if(preg_match("/\.php|\.PHP|\.js|\.JS/",$file)>0){
+	#script - go to that page
+	
+	getScript($file);
+}
 
 
 
