ea65266bc6431490bb51ac7d54b9291d269e61fa
jdickie <jgrantd@gmail.com>
2011-02-03 16:43:26 -0500

    Fixed issues with lag in ALR - ALR now exports shapes semi-efficiently into ImageTagger. May crash some slow connections, though. May want to install a LOADING shadowbox

--- PHP/parseRemoteJSON.php
+++ PHP/parseRemoteJSON.php
@@ -15,13 +15,36 @@ if(preg_match("/^<[A-Za-z]*/",$file)>0){
 }
 
 # create a unique ID 
-function genID(){
+function genLineID(){
+	$str=preg_split('/ /',"A B C D eE F G h i J ak L l M m N n O o P p Q q R r S s T t U u V v e8 9a 0i");
 	
+	$n=rand(0,1000);
+	$x=rand(0,(count($str)-1));
+	$id=$str[$x].$n;
+	while(in_array($id,$ids)){
+		$n=rand(0,1000);
+		$x=rand(0,(count($str)-1));
+		$id=$str[$x].$n;
+	}
+	array_push($ids,$id);
+	return $id;
 }
 # takes JSON PHP object
-function parseOut(json){
-	if(is_null(json)) die("Returned JSON is null");
+function parseOut($json){
 	
+	if(is_null($json)) die("Returned JSON is null");
+	#go through entire JSON to make sure lines have ID values attached
+	foreach($json['pages'] as $page){
+		#inside loop - looking at page
+		foreach($page['lines'] as $line){
+			#inner loop - looking at lines of $page
+			if((is_null($line['id'])||($line['id']=='undefined'))){
+				# generate line id
+				$line['id']=genLineId();
+			}
+		}
+	}
+	return $json;
 }
 
 # Gets contents of the [safe] url and 
@@ -32,12 +55,12 @@ function getData($url){
 	while(!feof($fp)){
 		$content.=fread($fp,8192);
 	}
-	
 	#check to make sure JSON string is parsed
 	#correctly
-	parseOut(json_decode($content));
+	$j=parseOut(json_decode($content));
 	
-	echo $content;
+	
+	echo json_encode($j);
 	#close stream
 	fclose($fp);
 }
