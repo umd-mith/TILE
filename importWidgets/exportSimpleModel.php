ff61b48a998588dbc52710d3c40248af30b28f00
jdickie <jgrantd@gmail.com>
2011-03-03 11:32:31 -0500

    Fixed static shape state bug

--- importWidgets/exportSimpleModel.php
+++ importWidgets/exportSimpleModel.php
@@ -24,6 +24,15 @@ function reverseJSONToSM($arr){
 						array_push($result['links'],$ref);
 					}
 					
+				} else if($key=='shapes') {
+					# save the SVG shape as a bounding box
+					$bb=convertShape($prop);
+					# push on to shapes stack
+					if(is_null($result['shapes'])){
+						$result['shapes']=array();
+					}
+					array_push($result['shapes'],$bb);
+					
 				} else {
 					#create new array or add to existing array
 					if(is_null($result[$key])){
@@ -48,7 +57,30 @@ function reverseJSONToSM($arr){
 		return $result;
 }
 
-
+# take a TILE shape object and break it down
+# to an object that describes a bounding box
+# width, height, x, y
+function convertShape($shpObj){
+	$result=array('id'=>$shpObj['id'],'width'=>0,'height'=>0,'x'=>0,'y'=>0);
+	foreach($shpObj as $key=>$prop){
+		if(preg_match('/posInfo/i',$key)){
+			# save all posInfo under EXTRA
+			$result['extra']=array();
+			# matched the position info of the shape - insert this
+			# into the result
+			foreach($prop as $k=>$o){
+				if(!(is_null($result[$k]))){
+					$result[$k]=$o;
+				}
+				$result[$k]=$o;
+			}
+			
+			
+		}
+	}
+	
+	return $result;
+}
 
 
 ?>
