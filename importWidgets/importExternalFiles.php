565bb761c2dbbc4a8ee4e1208ea2c43dca55cbe2
jdickie <jgrantd@gmail.com>
2011-03-08 16:47:20 -0500

    Updating the dashboard

--- importWidgets/importExternalFiles.php
+++ importWidgets/importExternalFiles.php
@@ -15,7 +15,7 @@ $f=$_GET['file'];
 # cancel the operation if this is an
 # unsafe file
 
-if((preg_match('/http\:\/\/|\.json|\.php|\.txt|\.xml/i',$f))==0){
+if((preg_match('/^[http\:\/\/]|\.json|\.php|\.txt|\.xml|\.html/i',$f))==0){
 	# something wrong with the file - not recognized
 	header('HTTP/1.0 415 Error file not recognized');
 	die();
@@ -27,7 +27,7 @@ $str='';
 if(preg_match('/\.xml/',$f)){
 	# use the import XML library
 	$res=convertXMLIntoTILEJSON($f,'text',0);
-} else if(preg_match('/http\:\/\//i',$f)){
+} else if(preg_match('/^http\:\/\//i',$f)){
 	# either a script or REST protocol - get file
 	# then determine the type of data returned
 	$str=getURL($f);
@@ -41,9 +41,17 @@ if(preg_match('/\.xml/',$f)){
 	
 	
 } else if(preg_match('/\{&\}/i',$f)>0) {
+	echo 'is a random var';
 	# just a string that needs to be parsed
 	$raw=inputJSON($f);
 	$res=parseStringIntoJSON($raw);
+} else if(preg_match('/\.html|^file\:\/\//',$f)>0){
+	if(preg_match('/^file\:/',$f)==0){
+		$f='file:/'.$f;
+	}
+	# is a file path - get contents
+	$raw=file_get_contents($f);
+	echo $raw;
 } else {
 	# something wrong with the file - not recognized
 	header('HTTP/1.0 415 Error file not recognized');
