2670fdaae6df7449be42919d343d4c0b48f0a65e
James Smith <jgsmith@gmail.com>
2011-03-29 13:36:32 -0500

    Simple converted tabs to spaces - nothing else

--- ImportExportScripts/coredata.php
+++ ImportExportScripts/coredata.php
@@ -1,143 +1,145 @@
 <?php
 class CoreData
 {
-	private $containers = array();
-	private $labels = array();
-	public  $current_container;
-	private $content;
-	
-	public function __construct($content) {
-		$this -> newContainer();
-		$this -> content = $content;
-		$this -> parse_content($content);
-	}
-	
-	// this function should be overridden to parse the content and create a set of collections with lines
-	//   and possibly links
-	public function parse_content($content) {
-		// we want to import $content as JSON for CoreData - other interpretations for other types
-		//$json = json_decode($content);
-		// loop through pages
-	}
-	
-	public function to_json($options = 0) {
-		// outputs a JSON representation of the containers and lines
-		$data = array();
-		$data['content'] = $this -> content;
-		$data['tile'] = array();
-		$data['tile']['pages'] = array();
-		
-		foreach($this -> containers as $container) {
-			if(!$container -> is_empty()) {
-				$page = array();
-				$page['url'] = $container -> image_url;
-				$page['lines'] = array();
-				$page['shapes'] = array();
-				
-				foreach($container -> lines as $line) {
-					$l = array();
-					$l['text'] = $line->text;
-					if($line->id != "") {
-						$l['id'] = $line->id;
-					}
-					$page['lines'][] = $l;
-				}
-				
-				foreach($container -> shapes as $shape) {
-					$s = array();
-					if($shape -> id != "") {
-						$s['id'] = $shape -> id;
-					}
-					$s['type'] = $shape -> type;
-					$s['color'] = $shape -> color;
-					$s['_scale'] = $shape -> scale;
-					$s['posInfo'] = $shape -> posInfo;
-					$page['shapes'][] = $s;
-				}
-				$data['tile']['pages'][] = $page;
-			}
-		}
-		$data['tile']['labels'] = array_values($this -> labels);
-		
-		return json_encode($data, $options);
-	}
-	
-	public function newContainer($url = "") {
-		$c = new CoreDataContainer($url);
-		$this -> containers[] = $c;
-		$this -> current_container = $c;
-	}
-	
-	public function addLabel($label, $object, $container = false) {
-		if(!$container) {
-			$container = $this -> current_container;
-		}
-		if($object -> id == "") {
-			return; // we can't add a label for an object with no id
-		}
-		if(array_key_exists($label, $this -> labels)) {
-			$this -> labels[$label]['selections'][] = $object -> id;
-		}
-		else {
-			$this -> labels[$label] = array();
-			$this -> labels[$label]['id'] = "lbl_" . (count($this -> labels) + 1);
-			$this -> labels[$label]['name'] = $label;
-			$this -> labels[$label]['selections'] = array();
-			$this -> labels[$label]['selections'][] = $object -> id;
-		}
-	}
+    private $containers = array();
+    private $labels = array();
+    public  $current_container;
+    private $content;
+    
+    public function __construct($content) {
+        $this -> newContainer();
+        $this -> content = $content;
+        $this -> parse_content($content);
+    }
+    
+    // this function should be overridden to parse the content and create a set of collections with lines
+    //   and possibly links
+    public function parse_content($content) {
+        // we want to import $content as JSON for CoreData - other interpretations for other types
+        $json = json_decode($content);
+        // each page is a container
+        
+        // loop through pages
+    }
+    
+    public function to_json($options = 0) {
+        // outputs a JSON representation of the containers and lines
+        $data = array();
+        $data['content'] = $this -> content;
+        $data['tile'] = array();
+        $data['tile']['pages'] = array();
+        
+        foreach($this -> containers as $container) {
+            if(!$container -> is_empty()) {
+                $page = array();
+                $page['url'] = $container -> image_url;
+                $page['lines'] = array();
+                $page['shapes'] = array();
+                
+                foreach($container -> lines as $line) {
+                    $l = array();
+                    $l['text'] = $line->text;
+                    if($line->id != "") {
+                        $l['id'] = $line->id;
+                    }
+                    $page['lines'][] = $l;
+                }
+                
+                foreach($container -> shapes as $shape) {
+                    $s = array();
+                    if($shape -> id != "") {
+                        $s['id'] = $shape -> id;
+                    }
+                    $s['type'] = $shape -> type;
+                    $s['color'] = $shape -> color;
+                    $s['_scale'] = $shape -> scale;
+                    $s['posInfo'] = $shape -> posInfo;
+                    $page['shapes'][] = $s;
+                }
+                $data['tile']['pages'][] = $page;
+            }
+        }
+        $data['tile']['labels'] = array_values($this -> labels);
+        
+        return json_encode($data, $options);
+    }
+    
+    public function newContainer($url = "") {
+        $c = new CoreDataContainer($url);
+        $this -> containers[] = $c;
+        $this -> current_container = $c;
+    }
+    
+    public function addLabel($label, $object, $container = false) {
+        if(!$container) {
+            $container = $this -> current_container;
+        }
+        if($object -> id == "") {
+            return; // we can't add a label for an object with no id
+        }
+        if(array_key_exists($label, $this -> labels)) {
+            $this -> labels[$label]['selections'][] = $object -> id;
+        }
+        else {
+            $this -> labels[$label] = array();
+            $this -> labels[$label]['id'] = "lbl_" . (count($this -> labels) + 1);
+            $this -> labels[$label]['name'] = $label;
+            $this -> labels[$label]['selections'] = array();
+            $this -> labels[$label]['selections'][] = $object -> id;
+        }
+    }
 }
 
 class CoreDataContainer {
-	public $lines = array();
-	public $shapes = array();
-	public $image_url = "";
-	
-	
-	public function __construct($url = "") {
-		$this -> image_url = $url;
-	}
-	
-	public function addLine($line) {
-		if($line != "") {
-			$l = new CoreDataLine($line);
-			$this -> lines[] = $l;
-			return $l;
-		}
-	}
-	
-	public function addShape($type, $color = "#000000", $scale = 1) {
-		$s = new CoreDataShape($type, $color, $scale);
-		$this -> shapes[] = $s;
-		return $s;
-	}
-	
-	public function is_empty() {
-		return count($this -> lines) == 0 && $this -> image_url == "";
-	}
+    public $lines = array();
+    public $shapes = array();
+    public $image_url = "";
+    
+    
+    public function __construct($url = "") {
+        $this -> image_url = $url;
+    }
+    
+    public function addLine($line) {
+        if($line != "") {
+            $l = new CoreDataLine($line);
+            $this -> lines[] = $l;
+            return $l;
+        }
+    }
+    
+    public function addShape($type, $color = "#000000", $scale = 1) {
+        $s = new CoreDataShape($type, $color, $scale);
+        $this -> shapes[] = $s;
+        return $s;
+    }
+    
+    public function is_empty() {
+        return count($this -> lines) == 0 && $this -> image_url == "";
+    }
 }
 
 class CoreDataLine {
-	public $text = "";
-	public $id = "";
-	
-	public function __construct($text, $id = "") {
-		$this -> id = $id;
-		$this -> text = $text;
-	}
+    public $text = "";
+    public $id = "";
+    
+    public function __construct($text, $id = "") {
+        $this -> id = $id;
+        $this -> text = $text;
+    }
 }
 
 class CoreDataShape {
-	public $id;
-	public $type;
-	public $scale;
-	public $color;
-	public $posInfo;
-	
-	public function __construct($type, $color = "#000000", $scale = 1) {
-		$this -> type = $type;
-		$this -> color = $color;
-		$this -> scale = $scale;
-	}
+    public $id;
+    public $type;
+    public $scale;
+    public $color;
+    public $posInfo;
+    
+    public function __construct($type, $color = "#000000", $scale = 1) {
+        $this -> type = $type;
+        $this -> color = $color;
+        $this -> scale = $scale;
+    }
 }
 ?>
--- ImportExportScripts/tei_p5_import.php
+++ ImportExportScripts/tei_p5_import.php
@@ -1,14 +1,14 @@
 <?php
 class TEIP5Import extends XMLStreamImport 
 {
-	public function __construct($content) {
-		$this -> setMilestoneXPath('//pb');
-		$this -> setDocumentStartXPath('//pb');
-		$this -> setImageUrlXPath('//pb/@facs');
-		// We divide lines based on elements that are one, two, or ... levels deep within the body
-		$this -> setLineStartXPath('//body/*|//body/*/*|//body/*/*/*|//body/*/*/*/*');
-		$this -> break_lines_on_newline = true;
-		parent::__construct($content);
-	}
+    public function __construct($content) {
+        $this -> setMilestoneXPath('//pb');
+        $this -> setDocumentStartXPath('//pb');
+        $this -> setImageUrlXPath('//pb/@facs');
+        // We divide lines based on elements that are one, two, or ... levels deep within the body
+        $this -> setLineStartXPath('//body/*|//body/*/*|//body/*/*/*|//body/*/*/*/*');
+        $this -> break_lines_on_newline = true;
+        parent::__construct($content);
+    }
 }
 ?>
