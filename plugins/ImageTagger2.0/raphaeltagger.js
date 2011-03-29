725b20ab54642a3a129dc724396f875438939b9e
jdickie <jgrantd@gmail.com>
2011-03-29 15:17:47 -0400

    Moved plugins folder to root

---
+++ plugins/ImageTagger2.0/raphaeltagger.js
@@ -0,0 +1,19 @@
+var RaphaelTagger=function(){
+	var self=this;
+	// initially set up the canvas
+	self.container='';
+	
+	self.paper=new Raphael();
+	
+	
+	
+};
+RaphaelTagger.prototype={
+	setUpCanvas:function(url){
+		// take url and set up canvas
+		
+		
+	}
+	
+	
+};
