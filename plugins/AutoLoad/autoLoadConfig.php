25bba170b4ad292f6b6d81fde2673b4d1985166c
jdickie <jgrantd@gmail.com>
2011-03-29 15:32:59 -0400

    Changed plugin.js files to tileplugin.js

--- plugins/AutoLoad/autoLoadConfig.php
+++ plugins/AutoLoad/autoLoadConfig.php
@@ -3,7 +3,7 @@
 
 $host='http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
 
-$prefix=preg_replace('/lib\/Plugins\/AutoLoad\/autoLoadConfig\.php/','',$host);
+$prefix=preg_replace('/plugins\/AutoLoad\/autoLoadConfig\.php/','',$host);
 // $prefix=preg_replace('/\//','\/',$prefix);
 
 # SET DEFAULT SCRIPT HERE
