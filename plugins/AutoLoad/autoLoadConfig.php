e31f97f090259dec8ada55197e63221ed25e8035
jdickie <jgrantd@gmail.com>
2011-06-14 14:44:17 -0400

    Deleting unneeded plugins and files

--- plugins/AutoLoad/autoLoadConfig.php
+++ plugins/AutoLoad/autoLoadConfig.php
@@ -7,11 +7,9 @@ $prefix=preg_replace('/plugins\/AutoLoad\/autoLoadConfig\.php/','',$host);
 // $prefix=preg_replace('/\//','\/',$prefix);
 
 # SET DEFAULT SCRIPT HERE
-$DEFAULT_SCRIPT="".$prefix."importWidgets/importXML.php?uri=".$prefix."importWidgets/testPemb.xml&rname=text&rnum=0&ipath=http://www.nd.edu/~pemb25/Website-tv/IMT/";
+$DEFAULT_SCRIPT=$prefix."/plugins/CoreData/importDataScript.php?file=http://ella.slis.indiana.edu/~jawalsh/tmp/tile_tei_import_test/acs0000001-01-i010.xml";
 
-$swinburne=$prefix."/plugins/CoreData/importDataScript.php?file=http://ella.slis.indiana.edu/~jawalsh/tmp/tile_tei_import_test/acs0000001-01-i010.xml";
-
-echo $swinburne;
+echo $DEFAULT_SCRIPT;
 
 
 
