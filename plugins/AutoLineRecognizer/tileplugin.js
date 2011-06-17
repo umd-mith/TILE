d263640bd5208cfbde87b55a137368fb8b8ba6db
jdickie <jgrantd@gmail.com>
2011-06-17 11:41:35 -0400

    Changed resize property for ALR - only resize red box from se corner

--- plugins/AutoLineRecognizer/tileplugin.js
+++ plugins/AutoLineRecognizer/tileplugin.js
@@ -853,7 +853,7 @@
             }
         });
         self.DOM.resizable({
-            handles: ['br'],
+            handles: 'se',
             stop: function(e, ui) {
 
                 $("#regionBox").css({
