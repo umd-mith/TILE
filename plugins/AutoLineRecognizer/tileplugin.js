3b2bddd5a9c74c85b2c8d15f335f27a5eb851c76
James Smith <jgsmith@gmail.com>
2011-06-17 10:36:42 -0400

    invert grey scale for light text on dark background

--- plugins/AutoLineRecognizer/tileplugin.js
+++ plugins/AutoLineRecognizer/tileplugin.js
@@ -1457,6 +1457,9 @@
                         var blue = this.regionData.data[index + 2];
                         var alpha = this.regionData.data[index + 3];
                         var average = (30*red + 59*green + 11*blue) / 100; // Y'_601 standard for intensity
+						if(!AutoR.darkText) {
+							average = 255 - average;
+						}
                         adiff = Math.abs(average - threshold);
                         if (! (this.dotMatrix[j])) {
                             this.dotMatrix[j] = [];
